dfb35dba4fc0ddbb05925b147015073e68f0b821
mikeyeung <mikeyeung@mikeyeung-Lenovo-G460>
2014-03-10 13:38:02 +0800

    Add and modified the method of resize function

--- jquery-rrslider.js
+++ jquery-rrslider.js
@@ -29,7 +29,10 @@ $(function(){
         
         // Default configuration properties
         var defaults = {
-            speed:             1000
+            height:     '100%',
+            width:      '100%',            
+            speed:      1000,
+            imgCover:   false
         };
         
         // Extend options
@@ -41,49 +44,120 @@ $(function(){
             var obj = $(this);    
             
             // Qty of li variable
-            var qty = $("ul li", obj).length;
+            var qty = $('ul li', obj).length;
             
-            // CSS setting           
-            $('button', obj).css({
-                'top': (obj.height() / 2) - ($('button').height() / 2)
+            // Css setting
+            $(obj).css({
+                "height":   options.height,
+                "width":    options.width
             });
-
-
+            
             //----------------------------------------------------------------------
             // Functions Setting
-            // ---------------------------------------------------------------------    
+            // ---------------------------------------------------------------------             
+            
+            function adjust_button_top()
+            {
+                $('button', obj).css({
+                    'top': ($("ul", obj).height() / 2) - ($('button').height() / 2)
+                });                
+            }      
+            
+            function adjust_img(selector)
+            {
+                if(!options.imgCover === true)
+                {
+                    if($('img', selector).width() < $('img', selector).height() )
+                    {
+                        $('img', selector).css({
+                            'height':   "100%",
+                            'width':    "auto"
+                        });
+
+                    }
+                    else
+                    {
+                        $('img', selector).css({
+                            'height':   "auto",
+                            'width':    "100%"
+                        });
+                    }                
+
+
+                    $('img', selector).css({
+                        'left': ($("ul", obj).width() / 2) - ($('img', selector).width() / 2),
+                        'top': ($("ul", obj).height() / 2) - ($('img', selector).height() / 2)
+                    });                    
+                }
+                else
+                {
+                    var imgSrc=$('img', selector).attr('src');
+                    
+                    $('img', selector).css('display','none');
+                    $(selector).css({
+                        'background-image':     'url(' + imgSrc + ')',
+                        'background-position':  'center center',
+                        'background-repeat':    'no-repeat',
+                        'background-size':          'cover',
+                        '-webkit-background-size': 'cover',
+                        '-moz-background-size':     'cover',
+                        '-o-background-size':       'cover',
+                        'filter':               "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + imgSrc + "', sizingMethod='scale')",
+                        '-ms-filter':           "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + imgSrc + "', sizingMethod='scale')"
+                    });                    
+                }
+            }   
+
             
             //----------------------------------------------------------------------
-            // Execute
-            // ---------------------------------------------------------------------             
-
-            $(".previous", obj).on("click", function(){
+            // Execute and Flow Control
+            // ---------------------------------------------------------------------      
+            
+            // Initialize Button Location
+            adjust_button_top();
+            adjust_img($("ul li", obj).eq(qty-1));
+            
+            // Resize Button Location
+            $(window).resize(function(){                
+                adjust_button_top();  
+                adjust_img($("ul li", obj).eq(qty-1));
+            });
+            
+            // Previous Button Event
+            $(".previous", obj).on("click", function(){  
                 
+                // Set the Current li and move it to right
                 $("ul li", obj).eq(qty-1).animate({"left": $("ul", obj).width()}, options.speed, function(){
                     $(this).prependTo(this, obj).css("left","0px");    
-                });                 
-                
-                $("ul li", obj).eq(0).appendTo("ul", obj).css("left", - $("ul", obj).width()).animate({"left":"0px"}, options.speed);
-
-                
-            });
-            
-            $(".next", obj).on("click", function(){     
-                
-                
-                $("ul li", obj).eq(qty-1).animate({"left": - $("ul", obj).width()}, options.speed, function(){
-                    $(this).prependTo("ul", obj).css("left","0px");    
                 });
                 
+                // Adjust the previous img
+                adjust_img($("ul li", obj).eq(0));
+                
+                // Set the Previous li and move it frome left to right                
+                $("ul li", obj).eq(0).appendTo("ul", obj).css("left", - $("ul", obj).width()).animate({"left":"0px"}, options.speed);                 
+            
+            });
+            
+            // Next Button Event            
+            $(".next", obj).on("click", function(){    
+                
+                // Set the Current li and move it to left
+                $("ul li", obj).eq(qty-1).animate({"left": - $("ul", obj).width()}, options.speed, function(){
+                    $(this).prependTo("ul", obj).css("left","0px");  
+                });
+                
+                // Set the Next li and move it from right to left
                 $("ul li", obj).eq(qty-2).css("left", $("ul", obj).width()).animate({"left":"0px"}, options.speed);
- 
+
+                // Because it has to get the newest size that can adjust img, so
+                // it must set here 
+                adjust_img($("ul li", obj).eq(qty-2));
                 
             });
             
-
-
-
         });
+        
     };    
     
 });
